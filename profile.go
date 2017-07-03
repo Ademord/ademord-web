523199238eac8e88bad968ab26b6d4d95e6b91ac
Ademord <druidelf3@gmail.com>
2017-07-03 22:45:58 +0200

    First commit

---
+++ profile.go
@@ -0,0 +1,18 @@
+type Profile struct {}
+
+var Ademord = Profile {
+  // You can check out my code here:
+  Github: url.URL("http://github.com/ademord"),
+  
+  // You can totally stalk me at these places:
+  Email: "ademord@gmail.com",
+  Instagram: url.URL("http://instagram.com/ademord"),
+  Tumblr: url.URL("http://frade.tumblr.com")
+
+}
+  
+  
+  
+  
+
+  
